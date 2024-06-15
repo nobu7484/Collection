8934138d7f997b909dab2d16afe7a4433f8332e0
yukinobu.ueno <165757180+nobu7484@users.noreply.github.com>
2024-06-15 22:14:15 +0900

    確認

--- src/practice/Chapter5.java
+++ src/practice/Chapter5.java
@@ -9,11 +9,11 @@ public class Chapter5 {
 
 	public static void main(String[] args) {
 		List<Task> list1 = new ArrayList<>();
-		list1.add(new Task(LocalDate.of(2024,10,21),":牛乳を買う。"));
-		list1.add(new Task(LocalDate.of(2024,9,15),":●●社面談"));
-		list1.add(new Task(LocalDate.of(2024,12,4),":手帳を買う"));
-		list1.add(new Task(LocalDate.of(2024,8,10),":散髪に行く"));
-		list1.add(new Task(LocalDate.of(2024,11,9),":スクールの課題を解く"));
+		list1.add(new Task(LocalDate.of(2024,10,21),"牛乳を買う。"));
+		list1.add(new Task(LocalDate.of(2024,9,15),"●●社面談"));
+		list1.add(new Task(LocalDate.of(2024,12,4),"手帳を買う"));
+		list1.add(new Task(LocalDate.of(2024,8,10),"散髪に行く"));
+		list1.add(new Task(LocalDate.of(2024,11,9),"スクールの課題を解く"));
 	
 		 Collections.sort(list1, (task1, task2) -> task1.getDate().compareTo(task2.getDate()));
 				
